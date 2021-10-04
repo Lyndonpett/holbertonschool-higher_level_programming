968a54c783a4a830a77e5cd7893cd416b340523a
lyndonpett <lyndonpett98@gmail.com>
2021-10-04 09:36:03 -0500

    Updated to actually use new list

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     newList = my_list.copy()
-    for x in range(len(my_list)):
+    for x in range(len(newList)):
         if newList[x] == search:
             newList[x] = replace
 
