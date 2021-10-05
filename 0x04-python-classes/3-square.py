3c4c1ddefdd87b1cedeebc556f2a4d819e944eda
lyndonpett <lyndonpett98@gmail.com>
2021-10-05 09:44:34 -0500

    Added task 3

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""The use of this module is to create a class named Square"""
+
+
+class Square:
+    """This define class and instantiates a private attribute"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        squareArea = self.__size * self.__size
+        return squareArea
