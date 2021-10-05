e16e78c1c210afacd83a690d35b018b3b5507bdd
lyndonpett <lyndonpett98@gmail.com>
2021-10-05 09:15:55 -0500

    Added task 0

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The use of this module is to create a class named Square"""
+
+
+class Square:
+    """This class Instantiation with size w/o type or value verification"""
+
+    def __init__(self, size):
+        self.__size = size
