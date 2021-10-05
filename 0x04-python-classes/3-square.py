5e929f2fab6c6499dda1d56c2d76f1fa564baea1
lyndonpett <lyndonpett98@gmail.com>
2021-10-05 09:48:37 -0500

    Updated typo in task 3

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -10,15 +10,16 @@ class Square:
 
     @property
     def size(self):
-        return(self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
 
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+
         else:
             self.__size = value
 
