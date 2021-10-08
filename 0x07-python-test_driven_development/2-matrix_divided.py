8d4b4781e628c1b8ffe3865c3f329ad0faf00058
lyndonpett <lyndonpett98@gmail.com>
2021-10-08 11:02:42 -0500

    Added task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """Divides elements in a matrix by 2 decimal points"""
+
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        for i in row:
+            if type(i) not in [int, float]:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+
+    rowLength = len(matrix[0])
+    for row in matrix:
+        if len(row) != rowLength:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    # Handles div
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(number / div, 2) for number in list] for list in matrix]
