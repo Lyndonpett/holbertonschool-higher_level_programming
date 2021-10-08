7e8e34d03d84b4c9598f8625b2a65a59594197db
lyndonpett <lyndonpett98@gmail.com>
2021-10-08 11:07:13 -0500

    Added task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,18 +5,17 @@
 def matrix_divided(matrix, div):
     """Divides elements in a matrix by 2 decimal points"""
 
+    errormessage = "matrix must be a matrix (list of lists) of integers/floats"
+
     if type(matrix) is not list:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(errormessage)
 
     for row in matrix:
         if type(row) is not list:
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(errormessage)
         for i in row:
             if type(i) not in [int, float]:
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(errormessage)
 
     rowLength = len(matrix[0])
     for row in matrix:
