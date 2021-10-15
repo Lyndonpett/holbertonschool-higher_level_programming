bdf0af4fad06f9955bc6a745d64910d14f3d70d8
lyndonpett <lyndonpett98@gmail.com>
2021-10-15 11:48:59 -0500

    Adding task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines class Student"""
+
+
+class Student:
+    """Adding definition to class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        StringAttrs = {}
+
+        if attrs is not None:
+            for key in attrs:
+                if key in self.__dict__:
+                    StringAttrs[key] = self.__dict__[key]
+            return StringAttrs
+        else:
+            return vars(self)
