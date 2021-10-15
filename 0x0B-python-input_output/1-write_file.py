d8eff60a107af069fb41100b6003767263089764
lyndonpett <lyndonpett98@gmail.com>
2021-10-15 09:54:08 -0500

    Fixed encoding syntax

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """Writes and counts the characters of a string"""
 
-    with open(filename, "w", encoding="UTF-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
