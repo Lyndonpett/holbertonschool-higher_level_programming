05274fc1801d26ce105d1b5dd416ef0ecf1c9d66
lyndonpett <lyndonpett98@gmail.com>
2021-10-18 11:08:42 -0500

    Adding task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines class Square"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defining Square class inherited from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
