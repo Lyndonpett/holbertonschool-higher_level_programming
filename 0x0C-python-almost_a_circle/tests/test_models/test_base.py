885bf81d51eae8dc30a226f2c0f9879e99f85e1f
lyndonpett <lyndonpett98@gmail.com>
2021-10-19 12:15:53 -0500

    Updated pep8 test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -49,3 +49,6 @@ class TestBase(unittest.TestCase):
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(
             ['models/base.py', 'models/rectangle.py', 'models/square.py'])
+
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
