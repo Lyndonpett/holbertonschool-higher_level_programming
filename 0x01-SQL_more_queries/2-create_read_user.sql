b8c641f747030116b23d713a303802f19f3839c3
lyndonpett <lyndonpett98@gmail.com>
2021-11-03 12:30:21 -0500

    Fixing task 2

--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE if not EXISTS hbtn_0d_2;
 CREATE USER if not EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON user_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON user_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
