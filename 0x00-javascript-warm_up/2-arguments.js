31111869c6749efa2323f3dc21d47de17f9662ef
lyndonpett <lyndonpett98@gmail.com>
2022-01-06 09:37:23 -0600

    Adding task 2

---
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const myVar1 = 'No argument';
+const myVar2 = 'Argument found';
+const myVar3 = 'Arguments found';
+
+if (process.argv.length > 3) {
+  console.log(myVar3);
+} else if (process.argv.length === 3) {
+  console.log(myVar2);
+} else {
+  console.log(myVar1);
+}
