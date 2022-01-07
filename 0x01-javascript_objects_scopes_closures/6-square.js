a7185ef0b303982b51a71f3a72d3d6c9842d776f
lyndonpett <lyndonpett98@gmail.com>
2022-01-07 09:35:38 -0600

    Adding task 6

---
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const SquareOG = require('./5-square');
+
+class Square extends SquareOG {
+  charPrint (c) {
+    if (c == null) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('c'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
