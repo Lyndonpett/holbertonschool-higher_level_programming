91c9c6e094345ae62fe0f03ec3a1909aa246fc89
lyndonpett <lyndonpett98@gmail.com>
2022-01-11 12:46:28 -0600

    Space, lol?

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 
 request(process.argv[2], function (_error, response) {
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
