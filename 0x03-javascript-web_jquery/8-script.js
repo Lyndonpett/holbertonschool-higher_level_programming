a86aa058c37a978049184a905872975945a3a3cf
lyndonpett <lyndonpett98@gmail.com>
2022-01-11 11:28:26 -0600

    Adding task 8

---
+++ 0x03-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+const URL = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+$.getJSON(URL, function (data) {
+  const results = data.results;
+  $.each(results, function (key, value) {
+    $('#list_movies').append('<li>' + value.title + '</li>');
+  });
+});
